317f64b5aca6e844c6725a9beb9b1b44ba673372
Umakant Mukhiya <umakant.1mu@gmail.com>
2023-02-27 16:15:01 +0530

    home and about section completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,97 @@
-import React from 'react';
+import React from "react";
+//countUp
+import CountUp from "react-countup";
+//intersection observer hook
+import { useInView } from "react-intersection-observer";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20
+        lg:gap-y-0 h-screen">
+          {/* image */}
+          <motion.div
+          variants={fadeIn('right',0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{once: false, amount: 0.3}}
+            className="flex-1 bg-about 
+              bg-contain bg-no-repeat
+              h-[540px] mix-blend-lighten bg-top"
+          >
+
+          </motion.div>
+          {/* text */}
+          <motion.div 
+          variants={fadeIn('left',0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{once: false, amount: 0.3}}
+          className="flex-1">
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-4">I'm a Computer Science MTech. Student at NIT Durgapur.</h3>
+            <p className="mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            {/* stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12">
+              <div>
+                <div className="text-[40px] font-ternary text-gradient mb-2">
+                  { inView ? 
+                  <CountUp start={0} end = {13} duration = {3} /> :null
+                  }
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br />
+                  Experiance
+                </div>
+              </div>
+
+              <div>
+                <div className="text-[40px] font-ternary text-gradient mb-2">
+                  { inView ? 
+                  <CountUp start={0} end = {15} duration = {3} /> :null
+                  }
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Project <br />
+                  Completed
+                </div>
+              </div>
+
+              <div>
+                <div className="text-[40px] font-ternary text-gradient mb-2">
+                  { inView ? 
+                  <CountUp start={0} end = {12} duration = {3} /> :null
+                  }
+                  k+
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Satisfied <br />
+                  Client
+                </div>
+              </div>
+
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href = "#" className="text-gradient btn-link">My portfolio</a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
